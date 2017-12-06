ca9ee5888ba80a7c26b14cbd5937efc458c69e4e
leksyib <leksyib14@gmail.com>
2017-12-06 02:57:30 +0100

    Add react-navigator to navigate to login page

--- App.js
+++ App.js
@@ -1,14 +1,19 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { DrawerNavigator } from 'react-navigation';
 import Login from './src/Login'
 
 export default class App extends Component {
   render() {
     return (
-     <Login />
+      <AppDrawerNavigator style={styles.nav}/>
     );
   }
 }
+
+const AppDrawerNavigator = DrawerNavigator({
+  Login: {screen: Login}
+});
 
 const styles = StyleSheet.create({
   container: {
@@ -17,4 +22,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  nav: {
+    backgroundColor: '#5D4037'
+  }
 });
