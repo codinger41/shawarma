5f856ed8cf3c02ff0adb4d727373e904f35d63f9
leksyib <leksyib14@gmail.com>
2017-12-15 00:05:53 +0100

    Consume API endpoint to GET all products

--- src/components/products.js
+++ src/components/products.js
@@ -2,21 +2,37 @@ import React, { Component } from 'react';
 import {View,  StyleSheet, ScrollView, Image, TouchableHighlight } from 'react-native';
 import { Container, Content, Card, CardItem, Thumbnail, Text, Button, Icon } from 'native-base';
 
-let data;
-    fetch('http://shawarma01.herokuapp.com/products')  
-    .then(function(response) {
-    return response.json()
-    });
+
 
 export default class Products extends Component {
+    constructor(props){
+        super(props)
+    
+        this.state = {
+            products: []
+        }
+    }
+    componentWillMount() {
+        fetch("https://shawarma01.herokuapp.com/api/products")
+            .then((res) => res.json())
+            .then((res) => {
+            console.log(res);
+            this.setState({
+                products: res
+            });
+            })
+            .done();
+        }
+
     render(){
+        const Product = this.state.products
         return(
             <ScrollView style={styles.scroll}>
-            <Card style={{ backgroundColor: '#5D4037' }} dataArray={data}
-                renderRow={(data) =>
+            <Card style={{ backgroundColor: '#5D4037' }} dataArray={Product}
+                renderRow={(Product) =>
                     <CardItem style={{ backgroundColor: '#5D4037' }} >
-                        <Thumbnail source={data.image} />
-                        <Text style={styles.text}>{data.name}</Text>
+                        <Thumbnail source={require('../images/icon.png')} />
+                        <Text style={styles.text}>{Product.name}</Text>
                         <Button style={{backgroundColor: '#5D4037',marginLeft: 30}}
                         > 
                         <Icon  style={{ marginLeft: 80, color: '#fff' }}  name='trash'
