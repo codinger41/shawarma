4fa5d50f92d25308fbc701432ff06316f18bce71
leksyib <leksyib14@gmail.com>
2017-12-14 19:19:55 +0100

    Change price datatype to number

--- models/productModel.js
+++ models/productModel.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const ProductSchema = mongoose.Schema({
     name: {type: String, required: true},
-    price: {type: String, required: true},
+    price: {type: Number, required: true},
     dailySales: {type: Number, required: false, default: 0},
     WeeklySales: {type: Number, required: false, default: 0}
 },
